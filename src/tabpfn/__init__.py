cd4dc785d21761125f6d381a4a709a38a25b8260
LeoGrin <leo.grinsztajn@polytechnique.edu>
2025-01-31 15:11:10 +0100

    bump version + add __version__ attribute

--- src/tabpfn/__init__.py
+++ src/tabpfn/__init__.py
@@ -1,7 +1,14 @@
 from tabpfn.classifier import TabPFNClassifier
 from tabpfn.regressor import TabPFNRegressor
+from importlib.metadata import version
+
+try:
+    __version__ = version(__name__)
+except ImportError:
+    __version__ = "unknown"
 
 __all__ = [
     "TabPFNClassifier",
     "TabPFNRegressor",
+    "__version__",
 ]
