4624dbebec699a12ceecd6076e84adb0557c2524
mert-kurttutan <kurttutan.mert@gmail.com>
2025-02-13 10:46:30 +0100

    Implement get_total_memory for windows

--- src/tabpfn/model/memory.py
+++ src/tabpfn/model/memory.py
@@ -252,12 +252,10 @@ class MemoryUsageEstimator:
                     os.sysconf("SC_PAGE_SIZE") * os.sysconf("SC_PHYS_PAGES") / 1e9
                 )
             except AttributeError:
-                # TODO: `os.sysconf` does not exist on windows.
-                free_memory = cls.convert_units(
-                    default_gb_cpu_if_failed_to_calculate,
-                    "gb",
-                    "b",
-                )
+                from tabpfn.utils import get_total_memory_windows
+
+                free_memory = get_total_memory_windows()
+
             except ValueError:
                 warnings.warn(
                     "Could not get system memory, defaulting to"
--- src/tabpfn/utils.py
+++ src/tabpfn/utils.py
@@ -4,8 +4,10 @@
 
 from __future__ import annotations
 
+import ctypes
 import os
 import sys
+import typing
 import warnings
 from collections.abc import Sequence
 from pathlib import Path
@@ -786,3 +788,37 @@ def _transform_borders_one(
         )
 
     return logit_cancel_mask, descending_borders, borders_t
+
+
+# ref: https://github.com/microsoft/windows-rs/blob/c9177f7a65c764c237a9aebbd3803de683bedaab/crates/tests/bindgen/src/fn_return_void_sys.rs#L12
+# ref: https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/ns-sysinfoapi-memorystatusex
+class _MEMORYSTATUSEX(ctypes.Structure):
+    _fields_: typing.ClassVar = [
+        ("dwLength", ctypes.c_ulong),
+        ("dwMemoryLoad", ctypes.c_ulong),
+        ("ullTotalPhys", ctypes.c_ulonglong),
+        ("ullAvailPhys", ctypes.c_ulonglong),
+        ("ullTotalPageFile", ctypes.c_ulonglong),
+        ("ullAvailPageFile", ctypes.c_ulonglong),
+        ("ullTotalVirtual", ctypes.c_ulonglong),
+        ("ullAvailVirtual", ctypes.c_ulonglong),
+        ("ullAvailExtendedVirtual", ctypes.c_ulonglong),
+    ]
+
+
+# Terminology: Use memory to referent physical memory, swap for swap memory
+def get_total_memory_windows() -> float:
+    """Get the total memory of the system for windows OS, using windows API.
+
+    Returns:
+        The total memory of the system in GB.
+    """
+    # Initialize the structure
+    mem_status = _MEMORYSTATUSEX()
+    # need to initialize lenght of structure, see microsft docs above
+    mem_status.dwLength = ctypes.sizeof(_MEMORYSTATUSEX)
+
+    k32_lib = ctypes.windll.LoadLibrary("kernel32.dll")
+    k32_lib.GlobalMemoryStatusEx(ctypes.byref(mem_status))
+
+    return mem_status.ullTotalPhys / 1e9  # Convert bytes to GB
